eeeb8cdeb660a10a41d2050da4177a509926c3b7
shadiboi <saoutabachi@gmail.com>
2019-04-24 17:00:05 -0600

    four of seven route for User complete

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,10 +5,69 @@ const User       = require('../models/Users')
 
 
 
-// Index route
-router.get('/', (req, res) => {
-    res.render('users/index.ejs')
+// INDEX
+router.get('/', async (req, res) => {
+    try{ 
+const foundUsers = await User.find({})
+console.log(foundUsers)
+        
+        res.render('users/index.ejs', {
+            users: foundUsers
+        })
+
+    } catch (err) {
+    res.send(err)
+    }
+
 });
+
+
+//NEW
+
+router.get('/new', (req, res) => {
+    res.render('users/new.ejs');
+})
+
+
+//CREATE
+router.post('/', async (req, res)=>{
+    try{
+        const createdUser = await User.create(req.body)
+        console.log("////////////USER/////////////////")
+        console.log(createdUser)
+        console.log("////////////USER/////////////////")
+        res.redirect('/users')
+
+    } catch (err){
+        console.log('//////ERR////////')
+        console.log(err)
+        console.log('/////ERR/////////')
+        res.send(err)
+   }
+})
+
+//SHOW
+router.get('/:id', async (req, res) => {
+    try{
+    const foundUser = await User.findById(req.params.id)
+    console.log('/////FOUND USER/////////')
+    console.log(foundUser)
+    console.log('/////FOUND USER/////////')
+    res.render('users/show.ejs', {
+        user: foundUser
+    })
+
+    } catch (err){
+        res.send(err)
+    }
+
+})
+
+
+
+
+
+
 
 
 
